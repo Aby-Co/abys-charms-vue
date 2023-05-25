367dba48687251f252bedfe272e9a4e42fee55cf
Sylvester Das <sylvester.das@wizardtales.com>
2023-05-25 11:52:11 +0530

    Tailwind css added

---
+++ src/components/Welcome/WelcomeComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="relative h-screen flex flex-col items-center justify-center text-center text-white py-0 px-3">
+    <div class="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
+        <video class="min-w-full min-h-full absolute object-cover" src="https://files-accl.zohopublic.in/public/workdrive-public/download/qrw264a1666fd047449e18d4855f2c3abce68" type="video/mp4" autoplay muted loop></video>
+    </div>
+    <div class="video-content space-y-2">
+        <h1 class="font-light text-7xl drop-shadow-lg shadow-black">Aby's Charms</h1>
+        <h3 class="font-light text-2xl">Where you can find the perfect earrings to match any outfit</h3>
+    </div>
+  </section>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'WelcomeComponent'
+})
+</script>
+
+<style>
+.video-docker video {
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.video-docker::after {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: transparent;
+  z-index: 1;
+}
+
+.video-content {
+  z-index: 2;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Welcome></Welcome>
+  <div class="container mx-auto">
+    <!-- <h1 class="text-3xl font-sans font-bold text-gray-900">Aby's Charms</h1> -->
+    <p class="text-xl text-gray-600">
+      We sell a variety of earrings, including golden jhumkas, golden earrings, and oxidised jhumkas.
+    </p>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div class="card">
+        <img src="https://via.placeholder.com/300x300" alt="Golden Jhumka">
+        <h2 class="text-xl font-sans font-bold text-gray-900">Golden Jhumka</h2>
+        <p class="text-base text-gray-600">
+          A classic piece that never goes out of style.
+        </p>
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+      <div class="card">
+        <img src="https://via.placeholder.com/300x300" alt="Golden Earrings">
+        <h2 class="text-xl font-sans font-bold text-gray-900">Golden Earrings</h2>
+        <p class="text-base text-gray-600">
+          A delicate pair of earrings that will add a touch of glamour to any outfit.
+        </p>
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+      <div class="card">
+        <img src="https://via.placeholder.com/300x300" alt="Oxidised Jhumka">
+        <h2 class="text-xl font-sans font-bold text-gray-900">Oxidised Jhumka</h2>
+        <p class="text-base text-gray-600">
+          A unique and stylish pair of earrings that will make a statement.
+        </p>
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Welcome from '@/components/Welcome/WelcomeComponent.vue'
 
+// @ is an alias to /src
 export default {
+
   name: 'HomeView',
   components: {
-    HelloWorld
+    Welcome
   }
 }
 </script>

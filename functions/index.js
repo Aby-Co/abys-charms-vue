954e0689709008d59463be35f42262e633957ea7
Sylvester Das <sylvester.das@wizardtales.com>
2023-05-25 18:22:10 +0530

    feat: functions added

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,11 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+// const { initializeApp } = require("firebase-admin");
+
+// initializeApp();
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -17,3 +20,31 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const Razorpay = require('razorpay');
+
+exports.orderCreate = onRequest({ region: 'asia-south1', maxInstances: 10 }, async (request, response) => {
+  const amount = request.body.amount;
+  const currency = request.body.currency;
+  const method = request.method;
+
+  if (method !== 'POST') {
+    return response.status(404).end();
+  }
+
+  if (!amount || !currency) {
+    return response.status(400).send({ error: true, message: 'Input is missing' });
+  }
+
+  // Test id
+  const instance = new Razorpay({
+    key_id: 'rzp_test_4yoApRIfF9nSCZ',
+    key_secret: '1yJ3CPq3UwTtLa22MJVVYHxo',
+  });
+
+  const res = await instance.orders.create({ amount, currency })
+  logger.info(res);
+  logger.info('Order created', res);
+
+  return response.send({ res })
+})

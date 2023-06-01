cd2946f549c69dfda1148a1f67ed07df2477d588
Sylvester Das <sylvester.das@wizardtales.com>
2023-06-01 10:24:36 +0530

    feat: scroll to top smoothly

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,51 @@
     <NavBar />
     <div class="shadow-md">
       <router-view/>
-      </div>
+    </div>
     <div class="container pt-4">
       <Footer></Footer>
     </div>
   </div>
+  <div @click="scrollToTop" v-if="visible" class="sticky w-full flex justify-end bottom-0 pb-3 pr-5 transition">
+    <span class="material-icons bg-accent text-bg rounded-full p-2 z-50 cursor-pointer shadow-lg shadow-black">keyboard_arrow_up</span>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import Footer from '@/components/Footer/Footer.vue'
-import NavBar from './components/Header/NavBar.vue'
+import NavBar from '@/components/Header/NavBar.vue'
+
+let scrollListener
 
 export default defineComponent({
   name: 'AppView',
   components: {
     NavBar,
     Footer
+  },
+  setup () {
+    const visible = ref(window.pageYOffset > 500)
+
+    return { visible }
+  },
+  mounted () {
+    const self = this
+    scrollListener = addEventListener('scroll', async function () {
+      const visible = window.pageYOffset > 500
+      if (self.visible !== visible) {
+        self.visible = visible
+      }
+    })
+  },
+  unmounted () {
+    removeEventListener('scroll', scrollListener)
+    scrollListener = null
+  },
+  methods: {
+    scrollToTop: function () {
+      window.scroll({ top: 0, behavior: 'smooth' })
+    }
   }
 })
 </script>
